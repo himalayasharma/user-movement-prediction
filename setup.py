c5c630360210d7eb3bb60228a1c1c2930f4b0d2d
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-07-03 19:11:36 -0400

    Create skeleton

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Comparitive analysis of dimensionality reduction techniques for compression of real-world sensor data',
+    author='Himalaya Sharma',
+    license='',
+)
